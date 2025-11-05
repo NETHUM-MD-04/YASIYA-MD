909005c3cbec80581e53c676464c98e33db63de1
NETHUM-MD-04 <nethumoshada459@gmail.com>
2025-11-05 07:55:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~JutjDQ4L#JleJEs36rt-FSyCSRH6NOxAEnKrYyylkIxhPwYmi5yc'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~lcRRDaaT#SxUJJ6VzG-HQTgObIGqx2AOAeJOTKr_IaReIz8wu6Ew'
 };
